04ee06f4ba7ab9e8dc866da6f670a7591a3ba0dd
Ra7x <Ra7x@DESKTOP-HFJDELQ>
2023-02-23 09:49:52 +0100

    Commit 0:Saludo

---
+++ src/control_versiones/Control_Versiones.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package control_versiones;
+
+/**
+ *
+ * @author Ra7x
+ */
+public class Control_Versiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+    }
+    
+}
