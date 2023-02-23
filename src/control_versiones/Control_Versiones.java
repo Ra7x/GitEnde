91c44232b6a45d64b5cf046f21cc706dea728f40
Ra7x <Ra7x@DESKTOP-HFJDELQ>
2023-02-23 09:51:10 +0100

    Commit 1:Grupo

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -15,7 +15,7 @@ public class Control_Versiones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("1ºDAW");
     }
     
 }
